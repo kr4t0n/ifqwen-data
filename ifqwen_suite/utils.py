9c1c6e5c79a99cd11cb7313dce86fb734cc5a94c
kr4t0n <kyle.yang1995@gmail.com>
2023-12-26 14:47:34 +0800

    MAINT: update utils

--- ifqwen_suite/utils.py
+++ ifqwen_suite/utils.py
@@ -12,16 +12,19 @@ def extract_code_pattern(data: str) -> Optional[str]:
     """Extract code snippet"""
     codes = CODE_PATTERN.findall(data)
 
-    if len(codes) > 1:
+    if len(codes) != 1:
         # find multiple code snippets, may not be a complete solution
         # skip it
-        return None
+        return ""
 
     return codes[0]
 
 
 def extract_lang_pattern(data: str) -> str:
     """Extract coding language"""
+    if data == "":
+        return ""
+
     lang = LANG_PATTERN.findall(data)[0][3:]
 
     return lang
